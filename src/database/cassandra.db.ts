e92edda19e1a2edef9b09cca7664f840c7f90677
unknown <diegocardwr@gmail.com>
2022-11-23 12:45:15 -0500

    updated connect function

--- src/database/cassandra.db.ts
+++ src/database/cassandra.db.ts
@@ -1,25 +1,19 @@
-const cassandra = require('cassandra-driver');
+import {Client} from "cassandra-driver";
 
-require('dotenv').config();
-
-const client = new cassandra.Client({
+const client = new Client({
     contactPoints: [`${process.env.CASSANDRA_HOST}:${process.env.CASSANDRA_PORT}`],
     localDataCenter: process.env.CASSANDRA_DATACENTER,
     keyspace: 'messages',
     credentials: {
-        username: process.env.CASSANDRA_USERNAME,
-        password: process.env.CASSANDRA_PASSWORD   
+        username: process.env.CASSANDRA_USERNAME!,
+        password: process.env.CASSANDRA_PASSWORD!   
     }
 });
 
-(async ()=>{
-    await client.connect((err: Error)=>{
-        if(err){
-            console.log("Cannoct connect to cassandra service");
-        }else{
-            console.log("Cassandra connected");
-        }
-    });
-})();
+client.connect().then(()=>{
+    console.log("Cassandra connected");
+}).catch((err:Error)=>{
+    console.log("Cannot connect to cassandra: ", err.name);
+});
 
-module.exports = client;
+export default client;
