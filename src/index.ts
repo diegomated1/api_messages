606e605799d252d1880300989d9a4a5b59a97cd7
unknown <diegocardwr@gmail.com>
2022-11-23 12:51:04 -0500

    using message router and cors

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
 // IMPORTS
-const dotenv = require('dotenv');
-const express = require('express');
-const morgan = require('morgan');
+import dotenv from 'dotenv';
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
 const app = express();
 
 // CONFIG
 dotenv.config();
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(cors());
 
 // ROUTER
+import msg_router from './router/message.router.js';
+app.use('/api/', msg_router);
 
 // LISTEN
-app.listen(process.env.API_PORT, process.env.API_HOST, ()=>{
+app.listen(parseInt(process.env.API_PORT!), process.env.API_HOST!, ()=>{
     console.log(`Listen on http://${process.env.API_HOST}:${process.env.API_PORT}/`);
 });
