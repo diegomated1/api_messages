d591e36aa997acc24dd8cf7e2e941c4d9ac596ff
unknown <diegocardwr@gmail.com>
2022-11-25 17:47:46 -0500

    auth middleware

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken';
+import {Request, Response, NextFunction} from 'express';
+
+export const handle_auth = async (req: Request, res: Response, next: NextFunction)=>{
+    try{
+        const auth = req.get('Authorization');
+        if(auth===undefined) throw "There is no token in the header";
+
+        const token_bearer = auth.split(' ');
+        if(token_bearer[0].toLowerCase()!=='bearer') throw "invalid token";
+
+        var token = token_bearer[1];
+        var user = jwt.verify(token, process.env.JWT_SECRET!);
+        res.locals.user = user;
+        next();
+    }catch(error){
+        const err = error as Error;
+        if(err.message){
+            res.status(400).json({succes: false, error: 1, message: (error as Error).message});
+        }else{
+            res.status(401).json({succes: false, error: 1, message: error});
+        }
+    }
+}
