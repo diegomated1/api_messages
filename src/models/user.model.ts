b1dc4ec483de6846e450282938e4c49977ddf0de
unknown <diegocardwr@gmail.com>
2022-12-12 18:50:11 -0500

    changed mapper for direct querys

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,21 +1,66 @@
+import { IUsers_by_id } from './../interfaces/cassandra_interfaces/IUser';
 import client from "../database/cassandra.db.js";
-import { mapping } from "cassandra-driver";
+import { types } from "cassandra-driver";
 
-const userMapper = new mapping.Mapper(client, {
-    models: {
-        'Users': {
-            tables: ['users_by_id', 'users_by_email', 'users_by_username'],
-            mappings: new mapping.DefaultTableMappings(),
-            columns: {
-                id_user: 'id_user',
-                username: 'username',
-                custom_username: 'custom_username',
-                email: 'email',
-                password: 'password'
-            }
-        }
+class User{
+
+    user_columns:string
+
+    constructor() {
+        this.user_columns = 'id_user, username, custom_username, email, password, register_date';
     }
-});
-const userModel = userMapper.forModel('Users');
 
-export default userModel;
+    find_by_id(id_user:string):Promise<types.ResultSet>{
+        return new Promise(async(res, rej)=>{
+            try{
+                const query = `SELECT ${this.user_columns} FROM users_by_id WHERE id_user = ?`;
+                const query_result = await client.execute(query, [id_user]);
+                res(query_result);
+            }catch(error){
+                rej(error);
+            }
+        });
+    }
+
+    find_by_username(username:string):Promise<types.ResultSet>{
+        return new Promise(async(res, rej)=>{
+            try{
+                const query = `SELECT ${this.user_columns} FROM users_by_username WHERE username = ?`;
+                const query_result = await client.execute(query, [username]);
+                res(query_result);
+            }catch(error){
+                rej(error);
+            }
+        });
+    }
+
+    find_by_email(email:string):Promise<types.ResultSet>{
+        return new Promise(async(res, rej)=>{
+            try{
+                const query = `SELECT ${this.user_columns} FROM users_by_email WHERE email = ?`;
+                const query_result = await client.execute(query, [email]);
+                res(query_result);
+            }catch(error){
+                rej(error);
+            }
+        });
+    }
+
+    insert(data: IUsers_by_id){
+        return new Promise(async(res, rej)=>{
+            try{
+                const params = [
+                    data.id_user, data.username, data.custom_username, data.email,
+                    data.password, data.register_date    
+                ];
+                const query = `INSERT INTO users_by_id(${this.user_columns}) VALUES (?,?,?,?,?,?)`;
+                const query_result = await client.execute(query, params);
+                res(query_result);
+            }catch(error){
+                rej(error);
+            }
+        });
+    }
+}
+
+export default new User();
