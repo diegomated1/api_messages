c093e4a04e78051cae652748ee44f38e625e0914
unknown <diegocardwr@gmail.com>
2022-12-21 13:15:08 -0500

    messages cassandra queries

--- src/models/message.model.ts
+++ src/models/message.model.ts
@@ -5,21 +5,98 @@ import {Client, types} from 'cassandra-driver';
 import { mapping } from "cassandra-driver";
 
 
-const messagesMapper = new mapping.Mapper(client, {
-    models: {
-        'Messages': {
-            tables: ['messages_by_id', 'messages_by_channel'],
-            mappings: new mapping.DefaultTableMappings(),
-            columns: {
-                id_message: 'id_message',
-                id_channel: 'id_channel',
-                create_at: 'create_at',
-                content: 'content',
-                id_author: 'id_author'
-            }
-        }
-    }
-});
-const messageModel = messagesMapper.forModel('Messages');
+class Message{
+    private message_columns:String
 
-export default messageModel;
+    constructor(){
+        this.message_columns = 'id_message, id_channel, create_at, content, id_author';
+    }
+
+    find_by_id(id_message:string,id_channel:string):Promise<types.ResultSet>{
+        return new Promise(async(res, rej)=>{
+            try{
+                const query = `SELECT ${this.message_columns} FROM messages_by_id WHERE id_message = ? AND id_channel = ?`;
+                const query_result = await client.execute(query, [id_message, id_channel]);
+                res(query_result);
+            }catch(error){
+                rej(error);
+            }
+        });
+    }
+
+    find_by_channel(id_channel:string):Promise<types.ResultSet>{
+        return new Promise(async(res, rej)=>{
+            try{
+                const query = `SELECT ${this.message_columns} FROM messages_by_channel WHERE id_channel = ?`;
+                const query_result = await client.execute(query, [id_channel]);
+                res(query_result);
+            }catch(error){
+                rej(error);
+            }
+        });
+    }
+
+    insert(data:IMessage):Promise<types.ResultSet>{
+        return new Promise(async(res, rej)=>{
+            try{
+                const params = [
+                    data.id_message, data.id_channel, data.create_at, data.content, data.id_author
+                ];
+                const queries = [
+                    {
+                        query: `INSERT INTO messages_by_id(${this.message_columns}) VALUES (?,?,?,?,?)`, params
+                    },{
+                        query: `INSERT INTO messages_by_channel(${this.message_columns}) VALUES (?,?,?,?,?)`, params
+                    }
+                ]
+                const query_result = await client.batch(queries, {prepare: true});
+                res(query_result);
+            }catch(error){
+                rej(error);
+            }
+        });
+    }
+
+    remove(id_message:string,id_channel:string):Promise<types.ResultSet>{
+        return new Promise(async(res, rej)=>{
+            try{
+                const queries = [
+                    {
+                        query: `DELETE FROM messages_by_id WHERE id_message = ? AND id_channel = ?`,
+                        params: [id_message, id_channel]
+                    },{
+                        query: `DELETE FROM messages_by_channel WHERE id_channel = ? AND id_message = ?`,
+                        params: [id_channel, id_message]
+                    }
+                ]
+                const query_result = await client.batch(queries, {prepare: true});
+                res(query_result);
+            }catch(error){
+                rej(error);
+            }
+        });
+    }
+
+    update(id_message:string, id_channel:string, create_at:string, content:string):Promise<types.ResultSet>{
+        return new Promise(async(res, rej)=>{
+            try{
+                const queries = [
+                    {
+                        query: 'UPDATE messages_by_id SET content = ? WHERE id_message = ? AND id_channel = ? AND create_at = ?',
+                        params: [content, id_message, id_channel, create_at]
+                    },{
+                        query: 'UPDATE messages_by_channel SET content = ? WHERE id_channel = ? AND id_message = ? AND create_at = ?',
+                        params: [content, id_channel, id_message, create_at]
+                    }
+                ]
+                const query_result = await client.batch(queries, {prepare: true});
+                res(query_result);
+            }catch(error){
+                rej(error);
+            }
+        });
+    }
+
+}
+
+export default new Message();
