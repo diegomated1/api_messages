aa53c5d0b1a99feb48a35fd15853c47a215be951
unknown <diegocardwr@gmail.com>
2022-12-21 13:15:20 -0500

    users cassandra queries

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -4,7 +4,7 @@ import { types } from "cassandra-driver";
 
 class User{
 
-    user_columns:string
+    private user_columns:string
 
     constructor() {
         this.user_columns = 'id_user, username, custom_username, email, password, register_date';
@@ -14,7 +14,7 @@ class User{
         return new Promise(async(res, rej)=>{
             try{
                 const query = `SELECT ${this.user_columns} FROM users_by_id WHERE id_user = ?`;
-                const query_result = await client.execute(query, [id_user]);
+                const query_result = await client.execute(query, [id_user], {prepare: true});
                 res(query_result);
             }catch(error){
                 rej(error);
@@ -26,7 +26,7 @@ class User{
         return new Promise(async(res, rej)=>{
             try{
                 const query = `SELECT ${this.user_columns} FROM users_by_username WHERE username = ?`;
-                const query_result = await client.execute(query, [username]);
+                const query_result = await client.execute(query, [username], {prepare: true});
                 res(query_result);
             }catch(error){
                 rej(error);
@@ -38,7 +38,7 @@ class User{
         return new Promise(async(res, rej)=>{
             try{
                 const query = `SELECT ${this.user_columns} FROM users_by_email WHERE email = ?`;
-                const query_result = await client.execute(query, [email]);
+                const query_result = await client.execute(query, [email], {prepare: true});
                 res(query_result);
             }catch(error){
                 rej(error);
@@ -46,7 +46,7 @@ class User{
         });
     }
 
-    insert(data: IUsers_by_id){
+    insert(data: IUsers_by_id):Promise<types.ResultSet>{
         return new Promise(async(res, rej)=>{
             try{
                 const params = [
@@ -54,7 +54,7 @@ class User{
                     data.password, data.register_date    
                 ];
                 const query = `INSERT INTO users_by_id(${this.user_columns}) VALUES (?,?,?,?,?,?)`;
-                const query_result = await client.execute(query, params);
+                const query_result = await client.execute(query, params, {prepare: true});
                 res(query_result);
             }catch(error){
                 rej(error);
